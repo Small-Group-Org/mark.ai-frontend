56ee78376b841d65900ca04d59a12deba65dcc14
kshitizPratap <kshitiz100mit@gmail.com>
2025-05-20 23:36:30 +0530

    Updated FS

--- src/assets/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DashboardIcon } from './DashboardIcon';
-export { default as CalendarIcon } from './CalendarIcon';
-export { default as MindIcon } from './MindIcon';
-export { default as SettingsIcon } from './SettingsIcon';
