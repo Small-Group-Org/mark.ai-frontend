a943ef9ed173f9a6e6410100b749bf0efdc53f43
Saurabh Nandwal <saurabh21nandwal@gmail.com>
2025-06-21 18:11:39 +0530

    initialiseChatWithMark

--- client/src/components/dashboard/ChatPanel.tsx
+++ client/src/components/dashboard/ChatPanel.tsx
@@ -63,15 +63,15 @@ const ChatPanel = () => {
     }
 
     // Case 1: If onboarding is complete, always initialize chat with Mark
-    // if (isOnboardingComplete()) {
-    //   handleChatResponse(initialiseChatWithMark);
-    //   return;
-    // }
+    if (isOnboardingComplete()) {
+      handleChatResponse(initialiseChatWithMark);
+      return;
+    }
 
     // // Case 2: If onboarding is NOT complete, only initialize if no messages exist
-    // if (!isOnboardingComplete() && messages?.length === 0) {
-    //   handleChatResponse(initialiseChatWithMark);
-    // }
+    if (!isOnboardingComplete() && messages?.length === 0) {
+      handleChatResponse(initialiseChatWithMark);
+    }
   }, [isAuth, isLoadingHistory]);
 
   useEffect(() => {
